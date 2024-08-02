bea27305e7499d474b08d2194f8dea85c4ca049f
lebgoodyear <lebgoodyear@protonmail.com>
2024-08-02 12:54:33 +0100

    add function to get human choice from prompt

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // function to get computer's play of rock, paper or scissors
 function getComputerChoice() {
-    let randomNumber = Math.random()
-    let computerChoice
+    let randomNumber = Math.random();
+    let computerChoice;
     if (randomNumber < 1/3) {
-        return computerChoice = "Rock";
+        return computerChoice = "rock";
     } else if (randomNumber > 2/3) {
-        return computerChoice = "Paper";
+        return computerChoice = "paper";
     } else {
-        return computerChoice = "Scissors";
+        return computerChoice = "scissors";
     }
 }
+
+// function for player to choose rock, paper or scissors
+function getHumanChoice () {
+    let humanChoice = prompt("Choose rock, paper or scissors:").toLowerCase();
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        alert("Invalid choice. Please select one of Rock, Paper or Scissors.");
+        humanChoice = prompt("Choose Rock, Paper or Scissors:").toLowerCase();
+    }
+    return humanChoice
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
